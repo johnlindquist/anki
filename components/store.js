6ac762baa1a018f46af5ce329c26ed145830a0fb
John Lindquist <johnlindquist@computer.local>
2017-11-23 13:14:49 -0700

    What... am... I... doing?

--- components/store.js
+++ components/store.js
@@ -2,8 +2,44 @@ import { BehaviorSubject, Observable, Subject } from "rxjs"
 import * as R from "ramda"
 import * as L from "partial.lenses"
 import { request } from "universal-rxjs-ajax"
-
+import { createEventHandler } from "recompose"
 const log = R.bind(console.log, console)
+
+const {
+  handler: ajax,
+  stream: onAjax$
+} = createEventHandler()
+
+const ajax$ = onAjax$
+  .do(log)
+  .mergeMap(({ url, method, resLens, body }) =>
+    request({
+      url: `${ENDPOINT}${url}`,
+      method,
+      body
+    })
+      .pluck("response")
+      .map(resLens)
+  )
+
+const makeHttpService = ajax => ({
+  get: (url, resLens) => {
+    log({ url, resLens })
+    return ajax({ method: "GET", url, resLens })
+  },
+
+  patch: (url, body, resLens) => {
+    log({ url, body })
+    return ajax({
+      method: "PATCH",
+      url,
+      body,
+      resLens
+    })
+  }
+})
+
+const http = makeHttpService(ajax)
 
 const ENDPOINT = "http://localhost:3002/"
 
@@ -12,51 +48,49 @@ const state = {
   cards: []
 }
 
-const decks$ = request({
-  url: `${ENDPOINT}decks`,
-  method: "GET"
-})
-  .pluck("response")
-  .map(R.objOf("decks"))
+// const decks$ = request({
+//   url: `${ENDPOINT}decks`,
+//   method: "GET"
+// })
+//   .pluck("response")
+//   .map(R.objOf("decks"))
 
-const cards$ = request({
-  url: `${ENDPOINT}cards`,
-  method: "GET"
-})
-  .pluck("response")
-  .map(R.objOf("cards"))
+// const cards$ = request({
+//   url: `${ENDPOINT}cards`,
+//   method: "GET"
+// })
+//   .pluck("response")
+//   .map(R.objOf("cards"))
 
-const loadedData$ = Observable.zip(decks$, cards$)
-  .map(R.mergeAll)
-  .map(data => state => ({ ...state, ...data }))
+// const loadedData$ = Observable.zip(decks$, cards$)
+//   .map(R.mergeAll)
+//   .map(data => state => ({ ...state, ...data }))
 
 export const dispatcher$ = new Subject()
 
-//TODO: Refactor to `createEventHandler`
-const putter$ = new Subject()
-  .switchMap(({ deckName, card }) => {
-    return request({
-      url: `${ENDPOINT}cards/${card.id}`,
-      method: "PATCH",
-      body: card,
-      dataType: "json",
-      contentType: "application/json"
-    })
-  })
-  .pluck("response")
-  .map(card =>
-    L.assign(
-      ["cards", L.find(R.whereEq({ id: card.id }))],
-      card
-    )
-  )
-
 export const store$ = new BehaviorSubject(state)
-  .merge(loadedData$, dispatcher$, putter$)
+  .merge(dispatcher$, ajax$)
+  .do(log)
   .scan((state, fn) => fn(state))
   .shareReplay(1)
   .do(state => console.log({ state }))
 
 export const updateCard = ({ deckName, card }) => {
-  putter$.next({ deckName, card })
+  const url = `cards/${card.id}`
+  const body = card
+  const lens = c =>
+    L.assign(["cards", L.find(R.whereEq({ id: c.id }))], c)
+
+  http.patch(url, body, lens)
 }
+
+setTimeout(() => {
+  http.get("decks", res => state => ({
+    ...state,
+    decks: res
+  }))
+  http.get("cards", res => state => ({
+    ...state,
+    cards: res
+  }))
+}, 3000)
